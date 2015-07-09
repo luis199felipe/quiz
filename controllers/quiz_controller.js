d7b04ea48d804effae9bb574cc4fede562431c61
luis199felipe <lufepa@live.com>
2015-07-09 21:23:05 +0200

    Base de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,13 @@
+var models = require('models');
+
 exports.question = function (req,res) {
-  res.render('quizes/question', { pregunta:'capital de italia'});
+  models.Quiz.findAll().then(function (quiz) {
+      res.render('quizes/question', { pregunta:quiz[0].pregunta});
+  });
 };
 
 exports.answer = function (req,res) {
-  if (req.query.respuesta === 'Roma') {
+  if (req.query.respuesta === quiz[0].respuesta) {
     res.render('quizes/answer', {respuesta:'Correcto'});
   }else {
     res.render('quizes/answer',{respuesta:'Incorrecto'});
