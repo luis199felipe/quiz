2818407e65620adad1d469bd23e420867139d991
luis199felipe <lufepa@live.com>
2015-07-10 20:51:33 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,16 @@
 var models = require('../models/models');
 
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else{next(new Error('No existe quizId=' + quizId));}
+    }
+  ).catch(function(error){next(error)});
+};
+
+
 // GET /users/:userId/quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(
@@ -11,23 +22,20 @@ exports.index = function(req, res) {
 // GET /quizes/:id
 exports.show = function(req, res) {
   models.Quiz.find(req.params.quizId).then(function (quiz) {
-      res.render('quizes/show', { quiz: quiz});
+      res.render('quizes/show', { quiz: req.quiz});
   })
 };
 
 exports.answer = function(req, res) {
   models.Quiz.find(req.params.quizId).then(function (quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
+
+    if (req.query.respuesta === req.quiz.respuesta) {
       res.render('quizes/answer',{
-         quiz: quiz, respuesta: 'Correcto'})
+         quiz: req.quiz, respuesta: 'Correcto'})
       }else {
         res.render('quizes/answer',{
-          quiz:quiz, respuesta:'Incorrecto'
+          quiz: req.quiz, respuesta:'Incorrecto'
         });
       }
     })
   };
-
-
-
-};
